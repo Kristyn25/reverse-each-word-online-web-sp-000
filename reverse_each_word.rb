ecc16e28e765525db39c9ab7767bd1881345ef69
Kristyn Little <kristynlittle@frontier.com>
2021-03-03 05:02:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(greeting)
   new_greeting = []
-  greeting.split.each do |reversed_words|
+  greeting.split.collect do |reversed_words|
     new_greeting << reversed_words.reverse
   end
   new_greeting.join(" ")
